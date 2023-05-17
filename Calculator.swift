3eea9886464b319919a1dea8cd411a4e179d4cad
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-05-17 20:45:50 +0200

    add Calculator.swift

---
+++ Calculator.swift
@@ -0,0 +1,11 @@
+class Calculator {
+    
+    func sum(primoNumero: Int, primoNumero: Int) -> Int {
+        return a + b
+    }
+    
+    func difference(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
